aae1d8d68a24774ed0edcaef4186d3c774cdbd56
sourav kumar <47256754+souravkr009@users.noreply.github.com>
2021-03-23 20:33:08 +0530

    Update onboadingModel.dart

--- lib/Models/onboadingModel.dart
+++ lib/Models/onboadingModel.dart
@@ -8,19 +8,19 @@ class OnboadingListItem {
           title: "Super Fast Pizza Delivery",
           subtitle:
               "The term pizza was first recorded in the 10th century in a Latin manuscript from the Southern Italian town of Gaeta in Lazio,",
-          image: "images/picture1.png",
+          image: "images/5.gif",
           color: Colors.green),
       OnboadingList(
           title: "Order Favorite Pizza",
           subtitle:
               "The term pizza was first recorded in the 10th century in a Latin manuscript from the Southern Italian town of Gaeta in Lazio,",
-          image: "images/picture2.jpg",
+          image: "images/4.gif",
           color: Colors.blue),
       OnboadingList(
           title: "Fresh Pizza At Home",
           subtitle:
               "The term pizza was first recorded in the 10th century in a Latin manuscript from the Southern Italian town of Gaeta in Lazio,",
-          image: "images/picture3.jpg",
+          image: "images/2.gif",
           color: Colors.orange),
     ];
 
